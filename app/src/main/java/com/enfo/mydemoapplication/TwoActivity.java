6ffa2044d904b0a0e9dfaae8021adc08fdae3331
liugao <359067070@qq.com>
2019-03-10 14:39:43 +0800

    测试

---
+++ app/src/main/java/com/enfo/mydemoapplication/TwoActivity.java
@@ -0,0 +1,22 @@
+package com.enfo.mydemoapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class TwoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        findViewById(R.id.te2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TwoActivity.this,MainActivity.class));
+
+            }
+        });
+    }
+}
